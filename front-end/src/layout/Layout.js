c82718d00b0de0a7c86569f7ca77d5dd57415584
TylerIsaac <tiisaac@comcast.net>
2022-01-13 18:05:00 -0500

    final push

--- front-end/src/layout/Layout.js
+++ front-end/src/layout/Layout.js
@@ -5,12 +5,11 @@ import Routes from "./Routes";
 import "./Layout.css";
 
 /**
- * Defines the main layout of the application.
- *
- * You will not need to make changes to this file.
+ * Defines main layout of the application.
  *
  * @returns {JSX.Element}
  */
+
 function Layout() {
   return (
     <div className="container-fluid">
